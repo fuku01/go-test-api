d3b32fad147177f6470c556c208a73fee5ade8b8
fuku <>
2023-07-18 15:08:24 +0900

    USERテーブルを作成し、TODOテーブルと関連付けを設定。#4

--- app/domain/model/todo.go
+++ app/domain/model/todo.go
@@ -6,4 +6,12 @@ import "gorm.io/gorm" // gormを使用するためのパッケージ
 type Todo struct {
 	gorm.Model        // gorm.Modelを埋め込むと、ID, CreatedAt, UpdatedAt, DeletedAtが自動で追加される
 	Content    string `json:"content"` // jsonのcontentと紐付け
+	UserID     uint   // *UserIDとの関連付け。TodoがUserを1つ持つことを表す。
+}
+
+// 「User」の型を定義
+type User struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Todos []Todo // *.Todosとの関連付け。UserがTodoを複数持つことを表す。
 }
